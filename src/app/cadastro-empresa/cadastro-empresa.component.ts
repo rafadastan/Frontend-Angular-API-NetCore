0fba59f16a6d07ee27e13ae4875e0a2b13c6c8a2
Raphael Augusto P <rafadastan02@live.com>
2020-11-03 21:39:50 -0300

    implementando a parte de cadastro e consulta de empresa

--- src/app/cadastro-empresa/cadastro-empresa.component.ts
+++ src/app/cadastro-empresa/cadastro-empresa.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 @Component({
@@ -9,6 +9,15 @@ import { environment } from '../../environments/environment';
 })
 export class CadastroEmpresaComponent implements OnInit {
 
+  //atributos
+  mensagemSucesso:string;
+  mensagemErro:string;
+
+  errosRazaoSocial = [];
+  errosNomeFantasia = [];
+  errosCnpj = [];
+
+  //injeção de dependência
   constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
@@ -16,16 +25,49 @@ export class CadastroEmpresaComponent implements OnInit {
 
   //função para executar o evento SUBMIT do formulário
   cadastrarEmpresa(formCadastro) : void {
+
+    this.mensagemSucesso = "Processando, por favor aguarde.";
+
+    this.errosNomeFantasia = [];
+    this.errosRazaoSocial = [];
+    this.errosCnpj = [];
     
     //realizando uma chamada HTTP POST para a API..
     this.httpClient.post(environment.apiEndpoint + '/empresas', formCadastro.form.value)
       .subscribe( //recuperando o promisse da chamada..
         (data:any) => { //retorno de sucesso da API
-          console.log(data);
+          //exibir a mensagem de sucesso obtida da API
+          this.mensagemSucesso = data.message;
+          //limpar os campos do formulário
+          formCadastro.form.reset();
         },
         (e:any) => { //recuperando o promisse de erro..
-          console.log(e);
+          
+          this.fecharMensagens();
+
+          //verificar o status do erro obtido (400, 550, etc..)
+          switch(e.status)
+          {
+            case 400:
+              
+              this.errosNomeFantasia = e.error.errors.NomeFantasia;
+              this.errosRazaoSocial = e.error.errors.RazaoSocial;
+              this.errosCnpj = e.error.errors.Cnpj;
+              break;
+
+            case 500:
+              
+              this.mensagemErro = e.error.message;
+              break;
+          }
         }
       );
+  }
+
+  //função para fechar as mensagens
+  fecharMensagens() : void {
+
+    this.mensagemSucesso = "";
+    this.mensagemErro = "";
   }
 }
--- src/app/consulta-empresa/consulta-empresa.component.ts
+++ src/app/consulta-empresa/consulta-empresa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-consulta-empresa',
@@ -7,9 +9,117 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaEmpresaComponent implements OnInit {
 
-  constructor() { }
+  //atributos
+  listagemEmpresas = []; //array vazio..
+  empresaEdicao = { id: '', nomeFantasia: '' };
 
+  errosNomeFantasia = []; //erros de validação
+
+  mensagemSucessoExclusao: string;
+  mensagemSucessoEdicao: string;
+
+  //declarando e inicializando o httpClient
+  constructor(private httpClient: HttpClient) { }
+
+  //método executado executado sempre que o componente é carregado
   ngOnInit(): void {
+    this.consultarEmpresas();
+  }
+
+  //função para executar a consulta na API e obter as empresas
+  consultarEmpresas(): void {
+
+    //executando uma requisição GET para a API..
+    this.httpClient.get(environment.apiEndpoint + "/empresas")
+      .subscribe(
+        (data: any[]) => {
+          //armazenando o resultado obtido no atributo
+          this.listagemEmpresas = data;
+        },
+        (e: any) => {
+          console.log(e);
+        }
+      )
+  }
+
+  //função para realizar a excusão da empresa
+  excluirEmpresa(id): void {
+
+    if (window.confirm('Deseja realmente excluir a empresa selecionada?')) {
+      //requisição de exclusão para a API..
+      this.httpClient.delete(environment.apiEndpoint + "/empresas/" + id)
+        .subscribe(
+          (data: any) => {
+            //capturando a mensagem de sucesso retornada pela API..
+            this.mensagemSucessoExclusao = data.message;
+            //executar a consulta de empresas..
+            this.consultarEmpresas();
+          },
+          (e: any) => {
+            console.log(e);
+          }
+        );
+    }
+
+  }
+
+  //função para obter 1 empresa pelo id
+  obterEmpresa(id): void {
+
+    this.fecharMensagens();
+    this.errosNomeFantasia = [];
+
+    //requisição na API para consultar 1 empresa baseado no id
+    this.httpClient.get(environment.apiEndpoint + "/empresas/" + id)
+      .subscribe(
+        (data: any) => {
+          this.empresaEdicao = data;
+        },
+        (e: any) => {
+          console.log(e);
+        }
+      )
+  }
+
+  //função para atualizar os dados da empresa
+  atualizarEmpresa(formEdicao): void {
+
+    this.mensagemSucessoEdicao = "Processando, por favor aguarde.";
+
+    this.errosNomeFantasia = [];
+
+    //realizando uma chamada HTTP POST para a API..
+    this.httpClient.put(environment.apiEndpoint + '/empresas', formEdicao.form.value)
+      .subscribe( //recuperando o promisse da chamada..
+        (data: any) => { //retorno de sucesso da API
+
+          this.fecharMensagens();
+
+          //exibir a mensagem de sucesso obtida da API
+          this.mensagemSucessoEdicao = data.message;
+          //executar novamente a consulta
+          this.consultarEmpresas();
+        },
+        (e: any) => { //recuperando o promisse de erro..
+
+          this.fecharMensagens();
+
+          //verificar o status do erro obtido (400, 550, etc..)
+          switch (e.status) {
+            case 400:
+
+              this.errosNomeFantasia = e.error.errors.NomeFantasia;
+              break;
+          }
+        }
+      );
+
+  }
+
+  //função para fechar as mensagens
+  fecharMensagens(): void {
+    this.mensagemSucessoExclusao = "";
+    this.mensagemSucessoEdicao = "";
   }
 
 }
